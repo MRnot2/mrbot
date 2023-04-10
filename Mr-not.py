a4a96d0c5b723598135334d1a7dc0e99b13da4cb
MRnot2 <116277951+MRnot2@users.noreply.github.com>
2023-04-10 14:30:34 +0500

    Update and rename README.md to Mr-not.py

---
+++ Mr-not.py
@@ -0,0 +1,11 @@
+# mrbot
+from random import *
+import os 
+u_pwd = input("Enter a password: ")
+pwd=['a','b','c','d','e','f','g','h','i','1','2','3','4','5','6']
+
+pw=" "
+while(pw!=u_pwd):
+	pwd=" "
+	for letter in range(len(u_pwd)):
+		
